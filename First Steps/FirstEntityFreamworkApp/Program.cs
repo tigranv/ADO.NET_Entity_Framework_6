e21aae8a6c372637c028b753689327509ae4dd20
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-08 00:50:27 +0400

    core

---
+++ First Steps/FirstEntityFreamworkApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstEntityFreamworkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDatabaseEntities contex = new MyDatabaseEntities();
+
+            // here table is like row
+
+            List<Table> list = contex.Tables.ToList();
+
+            foreach (Table item in list)
+            {
+
+            }
+        }
+    }
+}
