c356e700c1fe0bfee887b6471463017b4c327db7
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-08 01:49:16 +0400

    Model first finished

--- First Steps/Model First/Program.cs
+++ First Steps/Model First/Program.cs
@@ -12,9 +12,20 @@ namespace Model_First
         {
             using (MyModel1Container container = new MyModel1Container())
             {
+                container.PersonalInfoes.Add(new PersonalInfo {FirstName = "Levon", LastName = "Vardanyan", Age = 59 });
+                container.PersonalInfoes.Add(new PersonalInfo {FirstName = "Laura", LastName = "Mkrtchyan", Age = 61 });
+                container.SaveChanges();
 
+                var list = container.PersonalInfoes.ToList();
 
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"{item.Id} - {item.FirstName} {item.LastName} {item.Age}");
+                }
             }
+
+
+            Console.ReadKey();
         }
     }
 }
