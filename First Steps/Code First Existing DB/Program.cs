d94a8669a0a75b7d7645ec3d8c603991dc91ffa8
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-08 20:08:16 +0400

    Code first existing database

---
+++ First Steps/Code First Existing DB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First_Existing_DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
