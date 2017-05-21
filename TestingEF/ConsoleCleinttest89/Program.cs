f37465578a195290193669799fe2b34f5c54e885
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-21 21:14:01 +0400

    Test Client For CRM

---
+++ TestingEF/ConsoleCleinttest89/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCleinttest89
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BetC_CRM_DatabaseEntitiesTest89 db = new BetC_CRM_DatabaseEntitiesTest89();
+
+            var list = db.Partners.ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.EmailLists.Count);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestingEF/TestClientCRM/Entityes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClientCRM
+{
+    public class Partner
+    {
+        public Partner()
+        {
+            this.EmailLists = new HashSet<EmailList>();
+        }
+
+        public int PartnerID { get; set; }
+        public string FullName { get; set; }
+        public string CompanyName { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+
+        public virtual ICollection<EmailList> EmailLists { get; set; }
+    }
+
+    public partial class EmailList
+    {
+        public EmailList()
+        {
+            this.Partners = new HashSet<Partner>();
+        }
+
+        public int EmailListID { get; set; }
+        public string EmailListName { get; set; }
+
+        public virtual ICollection<Partner> Partners { get; set; }
+    }
+}
